e644803070dbc10730412b268b08a7af798322b2
scarf <greenscarf005@gmail.com>
2022-05-30 23:55:52 +0900

    fix: linux stuff

--- test.sh
+++ test.sh
@@ -3,26 +3,23 @@
 test_line () {
 	echo $@
 	echo $@ >> out.res
-	./microshell $@ >> out.res &
-	sleep .250
-	echo >> out.res
-	valgrind microshell > leaks.res 2> /dev/null
-	if grep "ROOT LEAK" < leaks.res > /dev/null 2> /dev/null ; then
-		printf "\e[0;31mLEAKS\n\e[0m"
-	fi
-	pid=$( pgrep microshell )
-	printf "\e[0;31m"
-	lsof -c microshell | grep $pid | grep -v cwd | grep -v txt | grep -v 0r | grep -v 1w | grep -v 2u | grep microshel
-	printf "\e[0m"
-	kill -9 $pid
-	wait $pid 2>/dev/null
+	valgrind --trace-children=yes --track-fds=yes --error-exitcode=1 \
+		./microshell 2>> leak.res
+	# (valgrind ./microshell $@ >> out.res 2>> leak.res) &
+	# pid=$!
+	# printf "\e[0;31m"
+	# lsof -c microshell | grep $pid | grep -v cwd | grep -v txt | grep -v 0r | grep -v 1w | grep -v 2u | grep microshell
+	# printf "\e[0m"
+	# kill -9 $pid
+	# wait $pid 2>/dev/null
 	#cat -e out.res > out
 }
 
 printf "\e[1;32mCompile\n"
-gcc -g -Wall -Werror -Wextra -DTEST_SH microshell.c -o microshell
+gcc -g -Wall -Werror -Wextra microshell.c -o microshell
 printf "\e[1;36mTest\n\e[0m"
-rm -f out.res leaks.res out
+rm -f out.res leak.res out
+test_line cd ..
 test_line /bin/ls
 test_line /bin/cat microshell.c
 test_line /bin/ls microshell.c
@@ -50,4 +47,4 @@ test_line ";" /bin/cat subject.fr.txt ";" /bin/cat subject.fr.txt "|" /usr/bin/g
 test_line blah "|" /bin/echo OK
 test_line blah "|" /bin/echo OK ";"
 printf "\e[1;32mDone\e[0m\n"
-rm -rf microshell.dSYM leaks.res
+# rm -rf microshell.dSYM leaks.res
