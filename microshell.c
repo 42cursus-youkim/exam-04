1e3ddb74435cd4fa620389a39e034d3705287e3b
scarf <greenscarf005@gmail.com>
2022-05-30 22:49:17 +0900

    refactor: use for loop instead of while, use enum for returns

--- microshell.c
+++ microshell.c
@@ -10,6 +10,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+typedef enum { Ok = 0, Error = -1 } res_t;
+
 void ft_write(int fd, char* msg, int size);
 
 void ft_putstr_fd(char* str, int fd) {
@@ -28,8 +30,8 @@ void ft_perror(char* msg, char* arg) {
   ft_write(2, "\n", 1);
 }
 
-void syscall_error_check(int ret) {
-  if (ret == -1) {
+void syscall_error_check(res_t ret) {
+  if (ret == Error) {
     ft_perror("error: fatal", NULL);
     kill(0, SIGINT);
     exit(1);
@@ -37,27 +39,27 @@ void syscall_error_check(int ret) {
 }
 
 void ft_write(int fd, char* msg, int size) {
-  int ret = write(fd, msg, size);
+  res_t ret = write(fd, msg, size);
   syscall_error_check(ret);
 }
 
 void ft_dup2(int srcfd, int dstfd) {
-  int ret = dup2(srcfd, dstfd);
+  res_t ret = dup2(srcfd, dstfd);
   syscall_error_check(ret);
 }
 
 void ft_close(int fd) {
-  int ret = close(fd);
+  res_t ret = close(fd);
   syscall_error_check(ret);
 }
 
 void ft_pipe(int* pipefd) {
-  int ret = pipe(pipefd);
+  res_t ret = pipe(pipefd);
   syscall_error_check(ret);
 }
 
 void ft_waitpid(pid_t pid, int* ws, int opt) {
-  int ret = waitpid(pid, ws, opt);
+  res_t ret = waitpid(pid, ws, opt);
   syscall_error_check(ret);
 }
 
@@ -71,13 +73,13 @@ void exec_builtin(char** args, char* envp[]) {
   (void)envp;
   if (!args[1] || args[2])
     ft_perror("error: cd: bad arguments", NULL);
-  if (chdir(args[1]) == -1)
+  if (chdir(args[1]) == Error)
     ft_perror("error: cd: cannot change directory to ", args[1]);
 }
 
 void exec_cmd(char* args[], char* envp[], int infd, int outfd) {
   int ws;
-  int ret;
+  res_t ret;
 
   (void)envp;
   if (*args == NULL)
@@ -91,7 +93,7 @@ void exec_cmd(char* args[], char* envp[], int infd, int outfd) {
     ft_dup2(infd, 0);
     ft_dup2(outfd, 1);
     ret = execve(args[0], args, NULL);
-    if (ret == -1) {
+    if (ret == Error) {
       ft_perror("error: cannot execute ", args[0]);
       kill(0, SIGINT);
       exit(1);
@@ -100,7 +102,6 @@ void exec_cmd(char* args[], char* envp[], int infd, int outfd) {
 }
 
 void exec_pipelines(char** args, char** envp) {
-  int i;
   int start;
   int* prevfd;
   int* currfd;
@@ -108,10 +109,9 @@ void exec_pipelines(char** args, char** envp) {
 
   prevfd = malloc(sizeof(int) * 2);
   currfd = malloc(sizeof(int) * 2);
-  i = -1;
   start = 0;
   prevfd[0] = 0;
-  while (args[++i]) {
+  for (int i = 0; args[i]; i++) {
     if (strcmp(args[i], "|") == 0) {
       ft_pipe(currfd);
       args[i] = 0;
@@ -141,13 +141,10 @@ void exec_cmds(char** args, char** envp) {
 }
 
 int main(int argc, char* argv[], char* envp[]) {
-  int i;
   int start;
 
-  (void)argc;
-  i = 0;
   start = 1;
-  while (argv[++i]) {
+  for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], ";") == 0) {
       argv[i] = 0;
       exec_cmds(argv + start, envp);
